b04f426614ebbbf6027cfe95be46509738439158
Dhiraj Kumar Mehta <dk266107@gmail.com>
2023-05-21 09:14:28 +0530

    Initial commit

---
+++ src/main/java/com/spring/jenkins/SpringBootDockerApplication.java
@@ -0,0 +1,27 @@
+package com.spring.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringBootDockerApplication.class);
+	
+	@PostConstruct
+	public void init()
+	{
+		logger.info("Application Started...");
+		
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application Executed...");
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
